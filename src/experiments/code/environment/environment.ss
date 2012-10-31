7e3d1ccba584ae806d2483c38051ea63ca9f3ffa
swathy <swathy@76ba195c-cacf-40f7-9991-3fb887e740ca>
2012-10-31 10:25:53 +0000

    git-svn-id: file:///labs/cse05/svn/popl-vlab@31 76ba195c-cacf-40f7-9991-3fb887e740ca

--- src/experiments/code/environment/environment.ss
+++
@@ -1,6 +0,0 @@
-#lang racket
-
-(require eopl/eopl)
-
-(require "env.ss" 
-	 "lookup.ss")
--- src/experiments/code/test/environment/environment-test.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket/base
- 
-(require rackunit "environment.ss" "lookup-test.rkt")
-
-(require rackunit/text-ui)
