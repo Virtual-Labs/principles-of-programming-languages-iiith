fa504768f5ebfb6fbf38ff489a65676ce926eea6
swathy <swathy@76ba195c-cacf-40f7-9991-3fb887e740ca>
2012-10-31 09:21:12 +0000

    git-svn-id: file:///labs/cse05/svn/popl-vlab@27 76ba195c-cacf-40f7-9991-3fb887e740ca

--- src/experiments/code/inductive-data-prog/inductive-data-prog.ss
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(require eopl/eopl)
-
-(require "factorial.ss")
-(require "factorial-acc.ss")
-(require "list-length.ss")
-(require "list-sum.ss")
--- src/experiments/code/test/inductive-data-prog/inductive-data-prog-test.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket/base
- 
-(require rackunit "inductive-data-prog.ss" "factorial-acc-test.ss" "factorial-test.ss" "list-length-test.ss" "list-sum-test.rkt")
-
-(require rackunit/text-ui)
-
