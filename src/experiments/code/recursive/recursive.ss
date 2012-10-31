dee69aaa6ba010baef26da2c0a34cbe8c662992b
swathy <swathy@76ba195c-cacf-40f7-9991-3fb887e740ca>
2012-10-31 09:44:43 +0000

    git-svn-id: file:///labs/cse05/svn/popl-vlab@29 76ba195c-cacf-40f7-9991-3fb887e740ca

--- src/experiments/code/recursive/recursive.ss
+++
@@ -1,8 +0,0 @@
-#lang scheme
-
-(require "recursive-ast.ss")
-(require "recursive-env.ss")
-(require "recursive-semanticdomain.ss")
-(require "recursive-eval.ss")
-(require "recursive-parser.ss")
-(require "recursive-run.ss")
--- src/experiments/code/test/recursive/recursive-test.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket/base
- 
-(require rackunit "recursive.ss" "recursive-ast-test.ss" "recursive-env-test.ss" "recursive-eval-test.ss" "recursive-parser-test.rkt" "recursive-run-test.rkt")
-
-(require rackunit/text-ui)
-
